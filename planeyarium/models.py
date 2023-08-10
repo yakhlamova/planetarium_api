90a4ea40a9971ee5b64a11aa78095960a543f5db
yakhlamova <yanaakhlamova@gmail.com>
2023-08-10 23:05:27 +0300

    Add validation in models

--- planeyarium/models.py
+++ planeyarium/models.py
@@ -1,5 +1,8 @@
 from django.conf import settings
+from django.core.exceptions import ValidationError
 from django.db import models
+from django.utils import timezone
+from pytz import utc
 
 
 class ShowTheme(models.Model):
@@ -13,6 +16,9 @@ class AstronomyShow(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     show_theme = models.ManyToManyField(ShowTheme)
+
+    class Meta:
+        ordering = ["title"]
 
     def __str__(self):
         return self.title
@@ -36,6 +42,40 @@ class ShowSession(models.Model):
     planetarium_dome = models.ForeignKey(PlanetariumDome, related_name="sessions", on_delete=models.CASCADE)
     show_time = models.DateTimeField()
 
+    class Meta:
+        ordering = ["-show_time"]
+
+    @staticmethod
+    def validate_show_time(show_time, error_to_raise):
+        now = timezone.now().astimezone(timezone.get_current_timezone())
+        if not (now < utc.localize(show_time)):
+            raise error_to_raise(
+                {
+                    "show_time": f"Show time must be lather than today!"
+                }
+            )
+
+    def clean(self):
+        ShowSession.validate_show_time(
+            self.show_time,
+            ValidationError
+        )
+
+    def save(
+            self,
+            force_insert=False,
+            force_update=False,
+            using=None,
+            update_fields=None,
+    ):
+        self.full_clean()
+        return super(ShowSession, self).save(
+            force_insert, force_update, using, update_fields
+        )
+
+    def __str__(self):
+        return self.astronomy_show.title + " " + str(self.show_time)
+
 
 class Reservation(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
@@ -43,11 +83,11 @@ class Reservation(models.Model):
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE
     )
 
-    def __str__(self):
-        return str(self.created_at)
-
     class Meta:
         ordering = ["-created_at"]
+
+    def __str__(self):
+        return str(self.created_at)
 
 
 class Ticket(models.Model):
@@ -60,3 +100,48 @@ class Ticket(models.Model):
     row = models.IntegerField()
     seat = models.IntegerField()
 
+    @staticmethod
+    def validate_ticket(row, seat, planetarium_dome, error_to_raise):
+        for ticket_attr_value, ticket_attr_name, planetarium_dome_attr_name in [
+            (row, "row", "rows"),
+            (seat, "seat", "seats_in_row"),
+        ]:
+            count_attrs = getattr(planetarium_dome, planetarium_dome_attr_name)
+            if not (1 <= ticket_attr_value <= count_attrs):
+                raise error_to_raise(
+                    {
+                        ticket_attr_name: f"{ticket_attr_name} "
+                                          f"number must be in available range: "
+                                          f"(1, {planetarium_dome_attr_name}): "
+                                          f"(1, {count_attrs})"
+                    }
+                )
+
+    def clean(self):
+        Ticket.validate_ticket(
+            self.row,
+            self.seat,
+            self.show_session.planetarium_dome,
+            ValidationError,
+        )
+
+    def save(
+            self,
+            force_insert=False,
+            force_update=False,
+            using=None,
+            update_fields=None,
+    ):
+        self.full_clean()
+        return super(Ticket, self).save(
+            force_insert, force_update, using, update_fields
+        )
+
+    def __str__(self):
+        return (
+            f"{str(self.show_session)} (row: {self.row}, seat: {self.seat})"
+        )
+
+    class Meta:
+        unique_together = ("show_session", "row", "seat")
+        ordering = ["row", "seat"]
