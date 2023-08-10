cdb57255000497a5218029f128a4198fc28d31cb
yakhlamova <yanaakhlamova@gmail.com>
2023-08-10 23:51:11 +0300

    Modify views

--- config/settings.py
+++ config/settings.py
@@ -9,18 +9,19 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv(BASE_DIR / ".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-093r^&a5^#-+kidvxoadd336x7(nt94txmf08n8vr_!d(mqc=='
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -40,7 +41,8 @@ INSTALLED_APPS = [
 
     "rest_framework",
 
-    "planeyarium"
+    "planeyarium",
+    "user",
 ]
 
 MIDDLEWARE = [
@@ -103,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+AUTH_USER_MODEL = "user.User"
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
--- planeyarium/views.py
+++ planeyarium/views.py
@@ -1,14 +1,19 @@
 from django.db.models import F, Count
 from django.shortcuts import render
+from datetime import datetime
 from rest_framework import viewsets, mixins, status
 from rest_framework.viewsets import GenericViewSet
+from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
+from planeyarium.permissions import IsAdminOrIfAuthenticatedReadOnly
 from planeyarium.models import (
     ShowTheme,
     AstronomyShow,
     ShowSession,
     PlanetariumDome,
-    Reservation, Ticket
+    Reservation,
 )
 
 from planeyarium.serializers import (
@@ -16,8 +21,12 @@ from planeyarium.serializers import (
     AstronomyShowSerializer,
     ShowSessionSerializer,
     PlanetariumDomeSerializer,
-    ReservationSerializer, AstronomyShowListSerializer, AstronomyShowDetailSerializer, ShowSessionListSerializer,
-    ShowSessionDetailSerializer
+    ReservationSerializer,
+    AstronomyShowListSerializer,
+    AstronomyShowDetailSerializer,
+    ShowSessionListSerializer,
+    ShowSessionDetailSerializer,
+    ReservationListSerializer
 )
 
 
@@ -28,6 +37,7 @@ class ShowThemeViewSet(
 ):
     queryset = ShowTheme.objects.all()
     serializer_class = ShowThemeSerializer
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
 
 class AstromyShowViewSet(
@@ -38,6 +48,28 @@ class AstromyShowViewSet(
 ):
     queryset = AstronomyShow.objects.prefetch_related("show_theme")
     serializer_class = AstronomyShowSerializer
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
+
+    @staticmethod
+    def _params_to_ints(qs):
+        """Converts a list of string IDs to a list of integers"""
+        return [int(str_id) for str_id in qs.split(",")]
+
+    def get_queryset(self):
+        """Retrieve the astronomy shows with filters"""
+        title = self.request.query_params.get("title")
+        show_themes = self.request.query_params.get("show_themes")
+
+        queryset = self.queryset
+
+        if title:
+            queryset = queryset.filter(title__icontains=title)
+
+        if show_themes:
+            show_themes_ids = self._params_to_ints(show_themes)
+            queryset = queryset.filter(show_themes__id__in=show_themes_ids)
+
+        return queryset.distinct()
 
     def get_serializer_class(self):
         if self.action == "list":
@@ -63,6 +95,22 @@ class ShowSessionViewSet(
         )
     )
     serializer_class = ShowSessionSerializer
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
+
+    def get_queryset(self):
+        date = self.request.query_params.get("date")
+        astronomy_show_id_str = self.request.query_params.get("astronomy_show")
+
+        queryset = self.queryset
+
+        if date:
+            date = datetime.strptime(date, "%Y-%m-%d").date()
+            queryset = queryset.filter(show_time__date=date)
+
+        if astronomy_show_id_str:
+            queryset = queryset.filter(movie_id=int(astronomy_show_id_str))
+
+        return queryset
 
     def get_serializer_class(self):
         if self.action == "list":
@@ -81,6 +129,12 @@ class PlanetariumDomeViewSet(
 ):
     queryset = PlanetariumDome.objects.all()
     serializer_class = PlanetariumDomeSerializer
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
+
+
+class ReservationPagination(PageNumberPagination):
+    page_size = 5
+    max_page_size = 100
 
 
 class ReservationViewSet(
@@ -93,4 +147,18 @@ class ReservationViewSet(
         "tickets__show_session__planetarium_dome"
     )
     serializer_class = ReservationSerializer
+    pagination_class = ReservationPagination
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        return Reservation.objects.filter(user=self.request.user)
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return ReservationListSerializer
+
+        return ReservationSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
 
